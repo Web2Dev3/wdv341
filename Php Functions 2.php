55941fe353c89d3e999efb02c40ce7f895795423
Web2Dev3 <61260404+Web2Dev3@users.noreply.github.com>
2020-09-08 15:26:04 -0500

    Add files via upload

---
+++ Php Functions 2.php
@@ -0,0 +1,42 @@
+<?php
+$departmentName = "DMACC Mailing Department";
+function dateFormat() {
+$date=date_create("2020-9-8");
+return date_format($date,"m/d/Y") . "<br>";
+}
+
+function dateInt() {
+$date=date_create("2020-9-8");
+return date_format($date,"d/m/Y") . "<br>";
+}
+
+function characterCount($inString)
+{
+	return 	strlen($inString) . "<br>";	
+}
+
+function lowerCase($inString) {
+return strtolower($inString) . "<br>";
+}
+
+function printNum() {
+$num1 = 1234567890;
+return number_format($num1) . "<br>";
+}
+
+function printCurrency() {
+$num2 = 123456;
+return "$" . number_format($num2, 2) . " USD";
+}
+?>
+<?php echo dateFormat(); ?>
+
+<?php echo dateInt(); ?>
+
+<?php echo characterCount($departmentName); ?>
+
+<?php echo lowerCase($departmentName); ?>
+
+<?php echo printNum(); ?>
+
+<?php echo printCurrency(); ?>
