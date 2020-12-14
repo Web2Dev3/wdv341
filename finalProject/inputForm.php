385d6a462daea23706ec4d3ec1708aa00f1cc4cc
Web2Dev3 <61260404+Web2Dev3@users.noreply.github.com>
2020-12-14 10:03:46 -0600

    Final Update

--- finalProject/inputForm.php
+++ finalProject/inputForm.php
@@ -348,55 +348,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 		//Form has not been seen by the user.  display the form
 	}// ends if submit 
 ?>
-<?php
-$target_dir = "images/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    $text= "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    $text= "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  $text= "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  $text= "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  $text= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  $text= "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    $text= "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-  } else {
-    $text= "Sorry, there was an error uploading your file.";
-  }
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -588,14 +539,7 @@ background-size: cover;
     <input type="reset" name="button2" id="button2" value="Reset" />
   </p>
 </form>
-<form class="form" action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-    <span class="errMsg"> <?php echo $text; ?></span>
-</form>
-
-        <?php
+       <?php
 			}//end else
         ?>    
 </div>
