339649ef40ba26414e9982e7fddf79089c6e1465
Web2Dev3 <61260404+Web2Dev3@users.noreply.github.com>
2020-12-14 10:02:10 -0600

    Final Update

--- finalProject/upload.php
+++ finalProject/upload.php
@@ -69,6 +69,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </div>
 <div class="col-lg-12" style="color: white; font-weight: bold; font-size: 1.5em;"><br>
 <?php
+$text= "";
 $target_dir = "images/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -78,45 +79,51 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    $text= "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    $text= "File is not an image.";
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  $text= "Sorry, file already exists.";
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  $text= "Sorry, your file is too large.";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $text= "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  $text= "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. Click the back arrow to go back.";
+    $text= "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    $text= "Sorry, there was an error uploading your file.";
   }
 }
 ?>
+<form class="form" action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Upload Image" name="submit">
+  <span><?php echo $text; ?></span>
+</form>
 </div>
     </div>
     </div>
